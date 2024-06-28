9713aa6478cf2a59f71c52533a8bd2f21772d36b
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-06-28 14:56:33 +0200

    Stampa stringhe del data

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+
     return view('welcome');
 });
 
 Route::get('/home', function () {
-    return view('welcome');
+    $data = [
+        "messaggio" => "Ciao",
+        "benvenuto" => "Benvenuti in Laravel"
+    ];
+    return view('welcome', $data);
 });
