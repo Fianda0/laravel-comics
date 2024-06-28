1f487e46060f48d9f7c128edce176611f6af9567
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-06-28 15:07:50 +0200

    stampa dati array in -pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,17 @@
 
 <body>
     <h1>{{ $benvenuto }}</h1>
-    <h2>{{ $messaggio }}</h2>
+
+    @foreach ($films as $film)
+        <div class="card">
+            <h3>{{ $film['title'] }}</h3>
+            <p>{{ $film['description'] }}</p>
+            <img src="{{ $film['thumb'] }}">
+            <p>Price: {{ $film['price'] }}</p>
+            <p>Date: {{ $film['sale_date'] }}</p>
+        </div>
+    @endforeach
+
 </body>
 
 </html>
